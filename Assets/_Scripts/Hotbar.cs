59d127771c2cd74da05979a4287f2a453f186125
RespectTeFallen <kd8ogd@hotmail.com>
2020-03-07 17:04:59 -0500

    V0.0.2

---
+++ Assets/_Scripts/Hotbar.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Hotbar : MonoBehaviour
+{
+
+    public List<GameObject> slots;
+    public List<Tile> tiles;
+    public static Tile setTile;
+
+    void Start()
+    {
+        setTile = tiles[0];
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            setTile = tiles[0];
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            setTile = tiles[1];
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            setTile = tiles[2];
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            setTile = tiles[3];
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            setTile = tiles[4];
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            setTile = tiles[5];
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha7))
+        {
+            setTile = tiles[6];
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha8))
+        {
+            setTile = tiles[7];
+        }
+    }
+}
---
+++ Assets/_Scripts/TileEditor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileEditor : MonoBehaviour
+{
+
+    public Tilemap world;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3Int cellPosition = world.layoutGrid.WorldToCell(pos);
+
+            world.SetTile(cellPosition, Hotbar.setTile);
+        }
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3Int cellPosition = world.layoutGrid.WorldToCell(pos);
+            
+            world.SetTile(cellPosition, null);
+        }
+    }
+}

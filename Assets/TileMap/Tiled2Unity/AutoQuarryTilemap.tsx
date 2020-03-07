84cb58f3816b11c707d831f9d80f532b0d81e622
RespectTeFallen <kd8ogd@hotmail.com>
2020-03-07 15:58:00 -0500

    V0.0.1

---
+++ Assets/TileMap/Tiled2Unity/AutoQuarryTilemap.tsx
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="AutoQuarryTilemap" tilewidth="32" tileheight="32" tilecount="6" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="TileMap/TileCoal.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="11.165" y="6.24483">
+    <polygon points="-1.79775,0.66233 -6.15021,4.82555 -4.25784,8.89415 -4.0686,14.5713 -1.13542,16.5582 3.69012,16.7475 6.52868,13.6251 10.9758,13.6251 12.7735,12.5843 12.7735,9.65109 9.74571,3.59551 3.69012,1.70313 1.70313,-0.378474"/>
+   </object>
+   <object id="2" x="19.7753" y="24.9793">
+    <polygon points="0,0 -1.79775,3.21703 -0.66233,5.77173 0.189237,6.71792 3.12241,6.71792 5.1094,4.82555 5.1094,1.98699 3.21703,-0.0946186"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="TileMap/TileGround.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="TileMap/TileGroundRocks1.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="TileMap/TileGroundRocks2.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="TileMap/TilePond.png"/>
+  <objectgroup draworder="index" id="4">
+   <object id="3" x="10.2188" y="2.83856">
+    <polygon points="0,0 -5.1094,7.09639 -7.19101,12.2058 -7.19101,23.0869 -6.33944,24.0331 -4.25784,25.0739 5.77173,28.0071 12.6789,28.1017 14.7605,26.0201 14.7605,23.9385 15.7067,19.9645 16.7475,16.7475 16.7475,9.27262 14.7605,7.09639 8.79953,6.05559 6.81254,3.12241 4.73093,0.189237"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="TileMap/TileGoldOre.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="15.8959" y="8.79953">
+    <polygon points="0,0 -5.01478,3.21703 -6.05559,7.38025 -5.86635,10.1242 -4.73093,14.0982 -2.93318,16.1798 1.13542,16.1798 5.86635,11.9219 7.00177,9.93495 7.09639,6.43406 6.05559,3.31165 4.92017,1.0408 2.93318,-0.0946186"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
